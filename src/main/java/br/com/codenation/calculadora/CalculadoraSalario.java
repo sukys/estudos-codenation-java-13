7d1089dfe33deeda176c3cfa1037bddf65d3b70f
Luis Sukys <luis.sukys@sippulse.com>
2020-06-22 06:36:03 -0300

    exercicio resolvido.

--- src/main/java/br/com/codenation/calculadora/CalculadoraSalario.java
+++ src/main/java/br/com/codenation/calculadora/CalculadoraSalario.java
@@ -1,21 +1,59 @@
 package br.com.codenation.calculadora;
 
-
 public class CalculadoraSalario {
 
+	public final double SALARIO_MINIMO = 1_039.00;
+
+	/**
+	 * Efetua o cálculo do salário, descontando o INSS e IRRF;
+	 * 
+	 * @param salarioBase
+	 * @return
+	 */
 	public long calcularSalarioLiquido(double salarioBase) {
-		//Use o Math.round apenas no final do método para arredondar o valor final.
-		//Documentação do método: https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html#round-double-
-		return Math.round(0.0);
+		if (salarioBase < SALARIO_MINIMO) {
+			salarioBase = 0.0;
+		} else {
+			salarioBase -= calcularInss(salarioBase);
+			salarioBase -= calcularIRRF(salarioBase);
+		}
+		return Math.round(salarioBase);
 	}
-	
-	
-	//Exemplo de método que pode ser criado para separar melhor as responsábilidades de seu algorítmo
+
+	/**
+	 * Retorna o valor do desconto do INSS referente ao salario Base informado.
+	 * 
+	 * @param salarioBase
+	 * @return
+	 */
 	private double calcularInss(double salarioBase) {
-		return 0.0;
+		double desconto = 0;
+		if (salarioBase <= 1500.00) {
+			desconto = salarioBase * 0.08;
+		} else if (salarioBase > 1500.00 && salarioBase <= 4000.00) {
+			desconto = salarioBase * 0.09;
+		} else {
+			desconto = salarioBase * 0.11;
+		}
+		return Math.round(desconto);
+	}
+
+	/**
+	 * Retorna o valor do desconto do IRRF referente ao salario Base informado.
+	 * 
+	 * @param salarioBase
+	 * @return
+	 */
+	private double calcularIRRF(double salarioBase) {
+		double desconto = 0;
+		if (salarioBase <= 3_000.00) {
+			desconto = 0;
+		} else if (salarioBase > 3_000.00 && salarioBase <= 6_000.00) {
+			desconto = salarioBase * 0.075;
+		} else {
+			desconto = salarioBase * 0.15;
+		}
+		return desconto;
 	}
 
 }
-/*Dúvidas ou Problemas?
-Manda e-mail para o meajuda@codenation.dev que iremos te ajudar! 
-*/
--- src/test/java/br/com/codenation/calculadora/CalculadoraSalarioTest.java
+++ src/test/java/br/com/codenation/calculadora/CalculadoraSalarioTest.java
@@ -7,8 +7,6 @@ import org.junit.Test;
 public class CalculadoraSalarioTest {
 
 	@Test
-	public void salarioLiquidoIsNotNull() {
-		assertNotNull(new CalculadoraSalario().calcularSalarioLiquido(1000.0));
-	}
+	public void salarioLiquidoIsNotNull() { assertNotNull(new CalculadoraSalario().calcularSalarioLiquido(1000.0)); }
 
 }
